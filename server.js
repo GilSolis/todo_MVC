06f2b9df698973f2090e729bc95926a4b7c88909
Gil Solis <gilsolisuw@gmail.com>
2021-04-13 20:48:56 -0700

    font-awesome tweak

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ connectDB()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+// app.use('/static', express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
